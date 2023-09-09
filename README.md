b7c32422e74f7fa1b8603246aa34caaba2f66693
Canrad <1517807724@qq.com>
2023-09-09 19:04:46 +0800

    Create basic frame

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # InfoSearcher
  Get the information pushed to users from WeChat official account.
 
+
+
 # Library
 WeChaty
+
+
+
+# Build
+
+1. install python and wechaty library
+
+```shell
+sudo apt-get install python
+pip install wechaty
+```
+
+2. run main.py
+
+```shell
+python main.py
+```
+
--- src/main.py
+++ src/main.py
@@ -1,16 +1,15 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+from wechaty import Wechaty
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+class MyBot(Wechaty):
+    async def on_message(self, msg):
+        from_contact = msg.talker()
+        text = msg.text()
+        await from_contact.say('Echo: ' + text)
 
 
-# 按间距中的绿色按钮以运行脚本。
+bot = MyBot()
+bot.start()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    print('PyCharm')
