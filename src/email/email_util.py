b47008b14465666a5dc3b51e49e3ad3684fa422c
Canrad <1517807724@qq.com>
2023-09-20 13:54:55 +0800

    添加保存邮件内容

--- src/email/email_util.py
+++ src/email/email_util.py
@@ -7,6 +7,7 @@ class EmailUtil:
     def __init__(self, username, password):
         self.username = username
         self.password = password
+        self.email_dict = {}
 
     def login(self):
         try:
@@ -29,19 +30,30 @@ class EmailUtil:
             for i in range(num_messages):
                 msg_lines = pop_server.top(i + 1, 0)[1]
                 subject = ''
+                content = ''
                 for line in msg_lines:
                     if line.startswith(b'Subject:'):
                         subject = line.decode('utf-8')
-                        break
+                    elif line.strip() != b'':  # 这里假设非空行即为内容
+                        content += line.decode('utf-8')
+                self.email_dict[subject] = content
                 print(f"邮件{i + 1}的主题：{subject}")
+
         except poplib.error_proto as e:
             print('获取邮件主题失败:', e)
 
         pop_server.quit()
 
+    def get_content_by_subject(self, subject):
+        if subject in self.email_dict:
+            return self.email_dict[subject]
+        else:
+            return None
+
 
 if __name__ == '__main__':
     # 授权码 ZWUXENDBVAWOHIZI
     e = EmailUtil('canrad7@163.com', 'ZWUXENDBVAWOHIZI')
-    e.login()
     e.get_email_subjects()
+    content = e.get_content_by_subject('test')
+    print(content)
