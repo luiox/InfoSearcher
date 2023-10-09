0a7ac8e5a7baae99c72f4b2d4456620b54c00750
Canrad <1517807724@qq.com>
2023-10-09 22:36:20 +0800

    temp

--- src/email/email_util.py
+++ src/email/email_util.py
@@ -5,32 +5,66 @@ import email
 import poplib
 import re
 from email import parser
+from email.header import decode_header
+
+# 对邮件字符串根据不同编码来解码
+def email_string_decode(str):
+    decode_str = ''
+    for part, charset in decode_header(subject):
+        if charset:
+            decode_str += part.decode(charset)
+        else:
+            decode_str += part
+    return decode_str
 
 
-# 通过邮件内容找到转发邮件的原始邮件
+# 通过邮件的message对象找到转发邮件的原始邮件，返回message对象
 def find_original_email(message):
-    if message.is_multipart():
-        for part in message.get_payload():
-            if part.get_content_type() == 'message/rfc822':
-                # 找到转发的原始邮件
-                original_email = part.get_payload(decode=True)
-                return original_email
-    else:
-        return None
+    original_email = None
+
+    # 遍历邮件的所有部分
+    for part in message.walk():
+        # 判断是否为文本类型的部分
+        if part.get_content_type() == 'text/plain':
+            # 获取文本内容
+            content = part.get_payload(decode=True).decode('utf-8')
+
+            # 在文本内容中查找原始邮件的引用符号
+            index = content.find('-----Original Message-----')
+            if index != -1:
+                # 截取引用符号之前的部分作为原始邮件内容
+                original_content = content[:index].strip()
+                print(original_content)
+                # 创建原始邮件的message对象
+                original_message = email.message_from_string(original_content)
+
+                # 设置原始邮件
+                original_email = original_message
+                break
+
+    return original_email
 
 
 # 传入一个message对象，判断是否是转发的邮件
 def is_forwarded_email(message):
-    subject = message['Subject']
-    sender = message['From']
+    subject = message.get('Subject', '')
+    decoded_subject = email_string_decode(subject)
+    # print("subject:" + decoded_subject)
+    body = ''
 
-    # 判断邮件主题中是否包含转发标识
-    if subject and ('FW:' in subject.upper() or 'FWD:' in subject.upper() or '转发：' in subject):
-        return True
+    if message.is_multipart():
+        for part in message.walk():
+            content_type = part.get_content_type()
+            if content_type == 'text/plain':
+                body += part.get_payload(decode=True).decode('utf-8')
+    else:
+        body = message.get_payload(decode=True).decode('utf-8')
 
-    # 判断发送者是否为常见的邮件转发服务
-    if sender and ('FORWARD' in sender.upper() or 'FWD' in sender.upper()):
-        return True
+    # 判断标题和正文中是否包含转发关键词
+    keywords = ['Fw:', 'FW:', 'Fwd:', '转发：']
+    for keyword in keywords:
+        if keyword in decoded_subject or keyword in body:
+            return True
 
     return False
 
@@ -77,21 +111,50 @@ class EmailUtil:
 
         pop_server.quit()
 
+    def get_email_message_object(self):
+        pop_server = self.login()
+        if not pop_server:
+            return
+
+        try:
+            num_messages = len(pop_server.list()[1])
+            for i in range(num_messages):
+                msg_lines = pop_server.top(i + 1, 0)[1]
+                msg_content = b'\r\n'.join(msg_lines).decode('utf-8')
+                msg = email.message_from_string(msg_content)
+                for line in msg_lines:
+                    if line.startswith(b'Subject:'):
+                        subject = line.decode('utf-8')
+                        subject = base64.b64decode(subject.replace('Subject: =?UTF-8?B?', '')).decode('utf-8')
+                # subject = msg.get('Subject')
+                content = ''
+                for part in msg.walk():
+                    if part.get_content_type() == 'text/plain':
+                        content += part.get_payload(decode=True).decode('utf-8')
+
+                self.email_dict[subject] = msg
+                print(f"邮件{i + 1}的主题：{subject}")
+
+        except poplib.error_proto as e:
+            print('获取邮件主题失败:', e)
+
+        pop_server.quit()
+
     def get_content_by_subject(self, subject):
         if subject in self.email_dict:
             return self.email_dict[subject]
         else:
             return None
 
-    def filter_subjects(self, keyword):
+    def filter_message_by_subjects(self, keyword):
         if not self.email_dict:
             raise Exception('还未获取邮件主题')
 
         try:
             filtered_dict = {}
-            for subject, content in self.email_dict.items():
+            for subject, message in self.email_dict.items():
                 if keyword in subject:
-                    filtered_dict[subject] = content
+                    filtered_dict[subject] = message
             return filtered_dict
         except re.error:
             raise Exception('正则表达式错误')
@@ -100,18 +163,18 @@ class EmailUtil:
 if __name__ == '__main__':
     # 授权码 ZWUXENDBVAWOHIZI
     e = EmailUtil('canrad7@163.com', 'ZWUXENDBVAWOHIZI')
-    e.get_email_subjects()
-    filtered_dict = e.filter_subjects('账单')
+    e.get_email_message_object()
+    filtered_dict = e.filter_message_by_subjects('账单')
     # 遍历获取的字典
-    for subject, content in filtered_dict.items():
+    for subject, message in filtered_dict.items():
         print("主题:", subject)
-        print("内容:", content)
-        # 将邮件内容转换为message对象
-        message = parser.Parser().parsestr(content)
+        # print("内容:", message.email_dict[subject])
         if is_forwarded_email(message):
             print("转发")
-            original_content = find_original_email(message)
-            original_message = email.message_from_bytes(original_content)
-            print(original_message)
+            original_message = find_original_email(message)
+            print("找到原邮件")
+            original_content = ''
+
+            print(original_content)
         else:
             print("不是转发")
