8088a8684f808987cddd95c7d7407556dc19627a
Canrad <1517807724@qq.com>
2023-10-12 21:50:17 +0800

    try to use imapclient

---
+++ src/email/email_rewrite.py
@@ -0,0 +1,21 @@
+from imapclient import IMAPClient
+
+# context manager ensures the session is cleaned up
+with IMAPClient(host="imap.163.com") as client:
+    client.login('canrad7@163.com', 'ZWUXENDBVAWOHIZI')
+    client.select_folder('INBOX')
+
+    # search criteria are passed in a straightforward way
+    # (nesting is supported)
+    messages = client.search(['NOT', 'DELETED'])
+
+    # fetch selectors are passed as a simple list of strings.
+    response = client.fetch(messages, ['FLAGS', 'RFC822.SIZE'])
+
+    # `response` is keyed by message id and contains parsed,
+    # converted response items.
+    for message_id, data in response.items():
+        print('{id}: {size} bytes, flags={flags}'.format(
+            id=message_id,
+            size=data[b'RFC822.SIZE'],
+            flags=data[b'FLAGS']))
